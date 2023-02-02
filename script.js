8eb56c08fae4adb13b832fb9e10b9b4407ad94ca
huntj0n <hunt.jon19@gmail.com>
2023-02-01 21:42:33 -0700

    handling click events

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 // console.log(document.querySelector(".message").textContent);
+/*
+//initial setup and selecting:
 document.querySelector(".message").textContent = "Correct Number!";
 
 document.querySelector(".number").textContent = 13;
@@ -7,3 +9,14 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 29;
 console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  //   document.querySelector('.message').textContent = 'Correct Number'
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!!";
+  }
+});
