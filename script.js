95c01e5ccf886bbab651d5b3f888b17882ee5390
huntj0n <hunt.jon19@gmail.com>
2023-02-02 23:55:36 -0700

    highscore fix

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".highscore").textContent = score;
   }
   //when guess is too high
   else if (guess > secretNumber) {
